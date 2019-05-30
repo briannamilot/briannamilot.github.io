a3dd781ffc0ceedd1387a41fd1743c1689083bb5
Nathan Milot <nathan.c.milot@gmail.com>
2019-05-30 13:25:06 -0700

    More improvements

--- js/script.js
+++ js/script.js
@@ -16,20 +16,20 @@
         var items = $(obj.children(), obj);
         items.each(function() {$(this).hide();})
         if(!o.child){var next = $(obj).children(':first');
-        }else{var next = o.child;
-        }
-        $(next).fadeIn(o.fadeSpeed, function() {
+      }else{var next = o.child;
+      }
+      $(next).fadeIn(o.fadeSpeed, function() {
         $(next).delay(o.pauseSpeed).fadeOut(o.fadeSpeed, function() {
           var next = $(this).next();
           if (next.length == 0){
             next = $(obj).children(':first');
           }
           $(obj).rotaterator({child : next, fadeSpeed : o.fadeSpeed, pauseSpeed : o.pauseSpeed});
-      })
+        })
       });
-      });
-    }
-  });
+    });
+  }
+});
 })(jQuery);
 
 
@@ -42,11 +42,11 @@ $(document).ready(function() {
 });
 
 function resizeResume(){
-    if($(window).height() < $(window).width()){
-      $('.resume_frame').css('height', $(window).height()*.6);
-    }else{
-      $('.resume_frame').css('height', $(window).width()*.6);
-    }
+  if($(window).height() < $(window).width()){
+    $('.resume_frame').css('height', $(window).height()*.6);
+  }else{
+    $('.resume_frame').css('height', $(window).width()*.6);
+  }
 };
 
 window.onscroll = function() {
@@ -58,9 +58,9 @@ window.addEventListener('click', hideMenuFunction);
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.getElementById("topBtn").style.display = "block";
+    document.getElementById("topBtn").style.display = "block";
   } else {
-      document.getElementById("topBtn").style.display = "none";
+    document.getElementById("topBtn").style.display = "none";
   }
 };
 
@@ -88,13 +88,31 @@ function hideAllCollapsed() {
 }
 
 function changeAllCollapsed(action) {
-  $("#collapseOne").collapse(action);
-  $("#collapseTwo").collapse(action);
-  $("#collapseThree").collapse(action);
-  $("#collapseAbout").collapse(action);
+  $("#about-collapse").collapse(action);
+  $("#experience-collapse").collapse(action);
+  $("#cv-collapse").collapse(action);
+  if(action == 'show') {
+    $(".arrow").addClass("rotated");
+  } else {
+    $(".arrow").removeClass("rotated");
+  }
+
 }
 
 function scrollToId(name) {
-  document.getElementById(name).scrollIntoView();
-  if(name != "contact") window.scrollBy(0, -70);
+  $("#" + name + "-collapse").collapse('show');
+  $("#" + name + "-arrow").addClass("rotated");
+  if(name != "contact" && !$("#" + name + "-collapse").hasClass("in")) {
+    setTimeout(function(){
+      document.getElementById(name).scrollIntoView();
+      if(name != "contact") window.scrollBy(0, -70);
+    }, 275);
+  } else {
+    document.getElementById(name).scrollIntoView();
+    if(name != "contact") window.scrollBy(0, -70);
+  }
+}
+
+function rotateArrow(name) {
+  $("#" + name + "-arrow").toggleClass("rotated");
 }
